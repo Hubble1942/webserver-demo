df902d741e3f69e295a7349f4617bac53616cb08
Christian Ewald <christian.ewald@teqable.com>
2024-08-07 15:08:55 +0200

    Setup project to meet QA criteria

---
+++ WebServerDemo.MinimalApi/WeatherForecast.cs
@@ -0,0 +1,14 @@
+// -----------------------------------------------------------------------
+// <copyright file="WeatherForecast.cs" company="Christian Ewald">
+// Copyright (c) Christian Ewald. All rights reserved.
+// Licensed under the MIT license.
+// See LICENSE in the project root for full license information.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace WebServerDemo.MinimalApi;
+
+internal sealed record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(this.TemperatureC / 0.5556);
+}
